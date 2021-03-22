1810f26e668e54e4d8ad4897ce9563397206bf5a
승핵 <sping__@naver.com>
2021-03-23 00:18:12 +0900

    피보나치 수열
    피보나치 수열

---
+++ 학교 실습/Fibonacci/Fibonacci/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a = 0, b = 1;
+int num = 0;
+void FibonacciFuc(int a,int b)
+{
+    a = a + b;
+    cout << a<<endl;
+    b = a + b;
+    cout << b;
+    cout << endl;
+    ++num;
+    if (num == 22)
+    {
+        cout << "끝";
+    }
+    else
+    {
+
+    FibonacciFuc(a, b);
+    }
+}
+
+int main()
+{
+    FibonacciFuc(a, b);
+}
+
