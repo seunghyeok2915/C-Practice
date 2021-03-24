42bd36afd85bd2728af84c5b5ebbe52fdebea1cb
승핵 <sping__@naver.com>
2021-03-24 09:49:15 +0900

    가우스 계산법 (재귀함수 사용)
    가우스 계산법 (재귀함수 사용)

---
+++ 학교 실습/가우스의계산법/가우스의 계산법(재귀함수 사용)/가우스의 계산법(재귀함수 사용)/가우스의 계산법(재귀함수 사용)/가우스의 계산법(재귀함수 사용).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Gausfuc(int a)
+{
+	if (a == 1)
+	{
+		return 1;
+	}
+	else
+		return Gausfuc(a - 1) + a;
+}
+int main()
+{
+	cout << Gausfuc(100);
+}
