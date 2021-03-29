e5e56d614fede2752880a0271e677663a60f215a
승핵 <sping__@naver.com>
2021-03-30 00:21:50 +0900

    제곱근 구하기
    제곱근 구하기

---
+++ 학교 실습/제곱근 구하기/제곱근 구하기/제곱근 구하기/제곱근 구하기.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+double sqrt(double a)
+{
+    double x = 71;
+
+    for (int i = 0; i < 10; i++)
+    {
+        x = (x + (a / x)) / 2;
+    }
+
+    return x;
+}
+
+void main()
+{
+    double a;
+
+    cin >> a;
+
+    double result = sqrt(a);
+
+    cout << result;
+
+}
