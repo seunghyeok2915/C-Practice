405520ebb5c1df2de0ab416ad965c7e38d5aa4a9
승핵 <sping__@naver.com>
2021-03-29 15:49:33 +0900

    1부터 n 까지 역순 출력
    1부터 n 까지 역순 출력

---
+++ 학교 실습/n 부터 1까지 역순출력/n 부터 1까지 역순출력/n 부터 1까지 역순출력/n 부터 1까지 역순출력.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+void ReverseNum(int n)
+{
+	if (n == 0)
+		return;
+	cout << n << endl;
+	--n;
+	ReverseNum(n);
+}
+
+int main()
+{
+	ReverseNum(100);
+}
