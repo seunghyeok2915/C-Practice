513d40ea1fe5c4bed43948654771d9992fe2ae20
승핵 <sping__@naver.com>
2021-03-29 15:50:52 +0900

    구구단 출력
    구구단 출력

---
+++ 학교 실습/구구단 출력/구구단 출력/구구단 출력/구구단 출력.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+    int x = 1;
+void Gugudan(int a, int b, int c)
+{
+    cout << a << " x " << x << " = " << a * x << endl;
+    ++x;
+    if (x == c)
+    {
+        ++a;
+        x = 1;
+        if(a == b)
+            return;
+    }
+
+    Gugudan(a, b, c);
+}
+int main()
+{
+    int a = 1, b = 10; // 1~ 9
+    int c = 10; // 9 까지 곱함
+    Gugudan(a, b, c);
+}
