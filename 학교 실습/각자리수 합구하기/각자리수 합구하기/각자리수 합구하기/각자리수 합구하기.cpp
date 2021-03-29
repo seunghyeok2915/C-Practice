1c560edca1f527d65daf7fdee69f3a861c46f51f
승핵 <sping__@naver.com>
2021-03-29 15:50:10 +0900

    각자리수 합구하기
    각자리수 합구하기

---
+++ 학교 실습/각자리수 합구하기/각자리수 합구하기/각자리수 합구하기/각자리수 합구하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int sum = 0;
+void SumOfnum(int n)
+{
+    if (n == 0)
+    {
+        cout << sum;
+        return;
+    }
+    sum += n % 10;
+    n /= 10;
+
+    SumOfnum(n);
+}
+
+int main()
+{
+    int a = 55215;
+    SumOfnum(a);
+}
