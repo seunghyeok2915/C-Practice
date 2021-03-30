921e3b87c49f749395bbadb5c9bf9e44c8ad6a17
승핵 <sping__@naver.com>
2021-03-30 22:18:40 +0900

    줄 바꿈
    줄 바꿈

--- 학교 실습/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력.cpp
+++ 학교 실습/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력/숫자 2개 입력받아 두수사이에있는 짝수출력.cpp
@@ -1,32 +1,32 @@
-﻿    #include <iostream>
-    int n = 0;
-    using namespace std;
-    void CoutOddNum(int a, int b)
-    {
-        if (n < a)
-        {
-            ++n;
-            CoutOddNum(a, b);
-        }
-        if (n > b)
-        {
-            return;
-        }
-        if (n % 2 == 0)
-        {
-            cout << n << " ";
-            ++n;
-            CoutOddNum(a, b);
-        }
-        else
-        {
-            ++n;
-            CoutOddNum(a, b);
-        }
-    }
-    int main()
-    {
-        int a = 1;
-        int b = 100;
-        CoutOddNum(a, b);
-    }
+﻿#include <iostream>
+int n = 0;
+using namespace std;
+void CoutOddNum(int a, int b)
+{
+	if (n < a)
+	{
+		++n;
+		CoutOddNum(a, b);
+	}
+	if (n > b)
+	{
+		return;
+	}
+	if (n % 2 == 0)
+	{
+		cout << n << " ";
+		++n;
+		CoutOddNum(a, b);
+	}
+	else
+	{
+		++n;
+		CoutOddNum(a, b);
+	}
+}
+int main()
+{
+	int a = 1;
+	int b = 100;
+	CoutOddNum(a, b);
+}
